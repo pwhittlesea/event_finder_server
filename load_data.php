f68e9a07cb8027b9631c6c775cfe27ef74f2b377
Phillip Whittlesea <pw.github@thega.me.uk>
2012-03-04 18:29:56 +0000

    Modified for cli usage
    Graphite updated
    Load modified to allow for additional endpoints

--- load_data.php
+++ load_data.php
@@ -9,50 +9,91 @@ include_once("lib/arc/ARC2.php");
 include_once("lib/graphite/graphite/Graphite.php");
 include_once("config/datastore.php");
 
-$endpoint = 'http://sparql.data.southampton.ac.uk/';
+// Read endpoint config from STDIN
+if (isset($argc) && $argc > 1) {
+    $config = parse_args($argv);
+}
+// Ensure there are endpoints
+if (!isset($config['endpoints'])) {
+    echo "No Endpoints Specified: Exiting\n";
+    exit();
+}
 
-// Fake time globals
-$time_start = mktime(0,0,0,9,2,2011);
-$time_end   = mktime(0,0,0,9,4,2011);
+// Ensure times are set
+if (!isset($config['time_start'])) {
+    $config['time_start'] = time();
+}
+if (!isset($config['time_end'])) {
+    $config['time_end'] = time() + (60 * 60 * 24 * 2); // 48 hours later
+}
 
-// TODO Replace with the OO method for requesting graphs
-$query = '
-PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX ev: <http://purl.org/NET/c4dm/event.owl#>
-PREFIX time: <http://purl.org/NET/c4dm/timeline.owl#>
-PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
-CONSTRUCT {
-  ?event rdfs:label ?label ; 
-         time:end ?end ;
-         time:start ?start ;
-         ev:time ?time ; 
-         geo:lat ?lat ; 
+foreach ($config['endpoints'] as $end) {
+    fetch_triples($end);
+}
+
+echo "Loading Complete\n";
+
+function fetch_triples($endpoint = null) {
+    global $config,$store;
+
+    // TODO Replace with the OO method for requesting graphs
+    $query = '
+    PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX ev: <http://purl.org/NET/c4dm/event.owl#>
+    PREFIX time: <http://purl.org/NET/c4dm/timeline.owl#>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    
+    CONSTRUCT {
+      ?event rdfs:label ?label ; 
+             time:end ?end ;
+             time:start ?start ;
+             ev:time ?time ; 
+             geo:lat ?lat ; 
+             geo:long ?long .
+    }
+    WHERE {
+      ?s geo:lat ?lat ; 
          geo:long ?long .
+      ?event ev:place ?s .
+      ?event rdfs:label ?label .
+      ?event ev:time ?timeOb .
+      ?timeOb time:end ?end .
+      ?timeOb time:start ?start .
+      FILTER ( 
+        xsd:dateTime(?end) > xsd:dateTime("'.date(DATE_W3C,$config['time_start']).'") &&
+        xsd:dateTime(?end) < xsd:dateTime("'.date(DATE_W3C,$config['time_end']).'") 
+      )
+    }';
+    
+    // Construct an arbitrary request string
+    $add = "?query=".urlencode( $query )."&output=rdfxml&jsonp";
+    
+    // Import the data into the local store
+    $store->query('LOAD <'. $endpoint . $add .'>');
+    
+    // Has anything gone wrong
+    if ($errs = $store->getErrors()) {
+        foreach ($errs as $err) {
+            echo "Error: ${err}\n";
+        }
+    } else {
+        echo "Endpoint: ${endpoint} loaded\n";
+    }
 }
-WHERE {
-  ?s geo:lat ?lat ; 
-     geo:long ?long .
-  ?event ev:place ?s .
-  ?event rdfs:label ?label .
-  ?event ev:time ?timeOb .
-  ?timeOb time:end ?end .
-  ?timeOb time:start ?start .
-  FILTER ( 
-    xsd:dateTime(?end) > xsd:dateTime("'.date(DATE_W3C,$time_start).'") &&
-    xsd:dateTime(?end) < xsd:dateTime("'.date(DATE_W3C,$time_end).'") 
-  )
-}';
 
-// Construct an arbitrary request string
-$add = "?query=".urlencode( $query )."&output=rdfxml&jsonp";
-
-// Import the data into the local store
-$store->query('LOAD <'. $endpoint . $add .'>');
-
-// Has anything gone wrong
-if ($errs = $store->getErrors()) {
-  echo var_dump($errs);
+function parse_args($args) {
+    $current_tag = "";
+    for ($i=1;$i<count($args);$i++) {
+        $arg = $args[$i];
+        if (substr($arg,0,2) == "--") {
+            $current_tag = substr($arg,2,strlen($arg));
+            $config[$current_tag] = "set";
+        } else {
+            $things = explode(",",$arg);
+            $config[$current_tag] = $things;
+        }
+    }
+    return $config;
 }
-
